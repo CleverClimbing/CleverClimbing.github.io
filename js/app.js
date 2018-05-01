f343f050082eefc75c35b888ebeccc7af89c247f
CleverClimbing <CleverClimbing@gmail.com>
2018-05-01 18:08:23 +0200

    Added display help for many devices

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
+window.onload = () => {
+    let menuOptions = $('.menu-option');
+    let linkCorrespondents = {
+      "Problems": $('.problem-scroll-wrapper'),
+      "Statistics": $('.statistics-scroll-wrapper'),
+      "Profile": $('.profile-scroll-wrapper')
+    }
 
-window.onload = addDomMethods;
+    $('.statistics-scroll-wrapper').hide();
+    $('.profile-scroll-wrapper').hide();
 
-function addDomMethods() {
-    
+    menuOptions.on('click', (e) => {
+      let self = $(e.target);
+      let currentActive = $('.active-link');
+
+      setActive(currentActive,self);
+
+      let linkCorrespondent = linkCorrespondents[self.text().trim()];
+      let oldLinkcorrespondent = linkCorrespondents[currentActive.text().trim()];
+
+      oldLinkcorrespondent.hide();
+      linkCorrespondent.show();
+    });
+}
+
+function setActive(oldLink,newLink) {
+  if(newLink[0] == oldLink[0]) return;
+  newLink.addClass('active-link');
+  oldLink.removeClass('active-link');
 }
